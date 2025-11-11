70dc79ae92ebc71ccf05e59a1dd3275fc33a339e
David Wong <david.wong@u.northwestern.edu>
2025-11-11 00:24:08 -0500

    full change to react with webpack compiling

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
+import Gazeval from "./routes/gazeval";
+import Home from "./routes/home";
+
+import "./app.css";
+
+// function Home() {
+//   return <h2>Home</h2>;
+// }
+
+// function About() {
+//   return <h2>About</h2>;
+// }
+
+export default function App() {
+  return (
+    <div>
+      {/* <nav>
+        <Link to="/">Home</Link> |{' '}
+        <Link to="/about">About</Link>
+      </nav> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gazeval" element={<Gazeval />} />
+        {/* you can add 404 route etc */}
+      </Routes>
+    </div>
+  );
+}
